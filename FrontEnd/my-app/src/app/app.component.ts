62cabf755ef481478614e480cf6230cf2995c821
James Murphree <jamesus95@gmail.com>
2017-09-30 14:13:41 -0700

    added http get

--- FrontEnd/my-app/src/app/app.component.ts
+++ FrontEnd/my-app/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'app';
+
+  results: string[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+  	this.http.get('url').subscribe(data => {
+  		this.results = data['results'];
+  	});
+  }
 }
